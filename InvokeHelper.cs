7e81ed25067a74d71257fefe187458026e9e8a60
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-07-25 16:03:35 +0200

    Trying out the new and amazing shared projects

--- InvokeHelper.cs
+++ InvokeHelper.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using Microsoft.CSharp.RuntimeBinder;
 using Binder = Microsoft.CSharp.RuntimeBinder.Binder;
+using System.Runtime.ExceptionServices;
 
 namespace Horizon
 {
@@ -189,7 +190,344 @@ namespace Horizon
 			return caller;
 		}
 
-	}
+
+
+        public static object FastInvoke(this Delegate @delegate, params dynamic[] args)
+        {
+            if (@delegate.Method.ReturnType != _voidType)
+            {
+                switch (args.Length)
+                {
+                    case 0:
+                        return ((dynamic)@delegate)();
+
+                    case 1:
+                        return ((dynamic)@delegate)(args[0]);
+
+                    case 2:
+                        return ((dynamic)@delegate)(args[0], args[1]);
+
+                    case 3:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2]);
+
+                    case 4:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3]);
+
+                    case 5:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4]);
+
+                    case 6:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5]);
+
+                    case 7:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+
+                    case 8:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+
+                    case 9:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+
+                    case 10:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+
+                    case 11:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10]);
+
+                    case 12:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]);
+
+                    case 13:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12]);
+
+                    case 14:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13]);
+
+                    case 15:
+                        return ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14]);
+
+
+                    default:
+                        try
+                        {
+                            return @delegate.DynamicInvoke(args.Cast<object>().ToArray());
+                        }
+                        catch (TargetInvocationException ex)
+                        {
+                            ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                switch (args.Length)
+                {
+                    case 0:
+                        ((dynamic)@delegate)();
+                        break;
+
+                    case 1:
+                        ((dynamic)@delegate)(args[0]);
+                        break;
+
+                    case 2:
+                        ((dynamic)@delegate)(args[0], args[1]);
+                        break;
+
+                    case 3:
+                        ((dynamic)@delegate)(args[0], args[1], args[2]);
+                        break;
+
+                    case 4:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3]);
+                        break;
+
+                    case 5:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4]);
+                        break;
+
+                    case 6:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5]);
+                        break;
+
+                    case 7:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+                        break;
+
+                    case 8:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+                        break;
+
+                    case 9:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+                        break;
+
+                    case 10:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+                        break;
+
+                    case 11:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10]);
+                        break;
+
+                    case 12:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]);
+                        break;
+
+                    case 13:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12]);
+                        break;
+
+                    case 14:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13]);
+                        break;
+
+                    case 15:
+                        ((dynamic)@delegate)(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14]);
+                        break;
+
+
+                    default:
+                        try
+                        {
+                            @delegate.DynamicInvoke(args.Cast<object>().ToArray());
+                        }
+                        catch (TargetInvocationException ex)
+                        {
+                            ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+                        }
+                        break;
+                }
+
+                return null;
+            }
+
+            throw new NotSupportedException("Invocation failure");
+        }
+
+
+
+        private static Type ResolveSignature(MethodInfo method, int argumentCounter)
+        {
+            var hasReturnType = method.ReturnType != _voidType;
+
+            if (method.IsStatic)
+            {
+                switch (argumentCounter)
+                {
+                    case 0:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object>)
+                            : typeof(Action<CallSite, Type>);
+
+                    case 1:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object>)
+                            : typeof(Action<CallSite, Type, object>);
+
+                    case 2:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object>);
+
+                    case 3:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object>);
+
+                    case 4:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object>);
+
+                    case 5:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object>);
+
+                    case 6:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object>);
+
+                    case 7:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object>);
+
+                    case 8:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object, object>);
+
+                    case 9:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object, object, object>);
+
+                    case 10:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object, object, object, object>);
+
+                    case 11:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object>);
+
+                    case 12:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object, object>);
+
+                    case 13:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object, object, object>);
+
+                    case 14:
+                        return hasReturnType
+                            ? typeof(Func<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>)
+                            : typeof(Action<CallSite, Type, object, object, object, object, object, object, object, object, object, object, object, object, object, object>);
+
+
+
+                    default:
+                        throw new NotSupportedException("Too many arguments.");
+                }
+            }
+
+
+            switch (argumentCounter)
+            {
+                case 0:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object>)
+                        : typeof(Action<CallSite>);
+
+                case 1:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object>)
+                        : typeof(Action<CallSite, object>);
+
+                case 2:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object>)
+                        : typeof(Action<CallSite, object, object>);
+
+                case 3:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object>);
+
+                case 4:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object>);
+
+                case 5:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object>);
+
+                case 6:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object>);
+
+                case 7:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object>);
+
+                case 8:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object>);
+
+                case 9:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object, object>);
+
+                case 10:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object, object, object>);
+
+                case 11:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object, object, object, object>);
+
+                case 12:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object, object, object, object, object>);
+
+                case 13:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object, object, object, object, object, object>);
+
+                case 14:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object, object, object, object, object, object, object>);
+
+                case 15:
+                    return hasReturnType
+                        ? typeof(Func<CallSite, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>)
+                        : typeof(Action<CallSite, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>);
+
+
+
+                default:
+                    throw new NotSupportedException("Too many arguments.");
+            }
+        }
+    }
 
 	static class InvokeHelper<T>
     {
